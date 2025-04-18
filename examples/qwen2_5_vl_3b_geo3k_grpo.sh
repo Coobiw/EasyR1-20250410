95216052818f2457c58a91e246ff9e99556dc8ed
coobiw <bowenqu@stu.pku.edu.cn>
2025-04-18 08:52:01 +0800

    update my pre-test workload

--- examples/qwen2_5_vl_3b_geo3k_grpo.sh
+++ examples/qwen2_5_vl_3b_geo3k_grpo.sh
@@ -1,12 +1,18 @@
 set -x
-
-MODEL_PATH=Qwen/Qwen2.5-VL-3B-Instruct  # replace it with your local file path
+NCCL_DEBUG=WARN 
+MODEL_PATH=/home/qbw/research/RL/EasyR1/cache/ckpt/Qwen2.5-VL-3B-Instruct  # replace it with your local file path
 
 python3 -m verl.trainer.main \
     config=examples/config.yaml \
-    data.train_files=hiyouga/geometry3k@train \
-    data.val_files=hiyouga/geometry3k@test \
+    data.train_files=/home/qbw/research/RL/EasyR1/cache/data/geometry3k@train \
+    data.val_files=/home/qbw/research/RL/EasyR1/cache/data/geometry3k@test \
+    data.max_pixels=1048576 \
+    data.min_pixels=262144 \
+    worker.actor.micro_batch_size_per_device_for_update=1 \
+    worker.actor.micro_batch_size_per_device_for_experience=4 \
     worker.actor.model.model_path=${MODEL_PATH} \
-    worker.rollout.tensor_parallel_size=1 \
-    trainer.experiment_name=qwen2_5_vl_3b_geo_grpo \
-    trainer.n_gpus_per_node=2
+    worker.actor.model.freeze_vision_tower=true \
+    worker.actor.model.enable_gradient_checkpointing=true \
+    worker.rollout.tensor_parallel_size=2 \
+    trainer.experiment_name=qwen2_5_vl_3b_geo3k_grpo \
+    trainer.n_gpus_per_node=4
